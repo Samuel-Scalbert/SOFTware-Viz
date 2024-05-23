fa757df8d9e6522723dbb561282823c84c755d23
Samuel-Scalbert <sam.scalbert@gmail.com>
2024-05-23 17:11:47 +0200

    Correction scriptjs

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -23,39 +23,55 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             document.querySelectorAll('.mention_search_doc_id').forEach(item => {
                 item.addEventListener('click', event => {
-                    console.log(item)
+                    const elements = document.querySelectorAll('.mention_doc_id');
+                    elements.forEach(element => {
+                        var dropdownBtn = element.querySelector('.dropbtn');
+                        var dropdownContent = element.querySelector('.dropdown-content');
+                        dropdownBtn.style.color = '';
+                        dropdownContent.style.display = 'none';
+                    });
+                    // Reset the color of the previously clicked element
+                    if (previousClickedElement !== null) {
+                        previousClickedElement.style.backgroundColor = ''; // Reset to default
+                    }
+
+                    // Set the background color of the current clicked element
+                    item.style.backgroundColor = 'red';
+
+                    // Set the current clicked element as the previous clicked element
+                    previousClickedElement = item;
+
+                    const idValue = item.getAttribute('id');
+                    handleClick(idValue, 1);
+                    resultBox.innerHTML = "<div class='dropdown-content-search'></div>";
                 });
             });
         }
     }
-
-   document.querySelectorAll('.structure').forEach(item => {
-    item.addEventListener('click', event => {
-        const elements = document.querySelectorAll('.mention_doc_id');
-        elements.forEach(element => {
-            var dropdownBtn = element.querySelector('.dropbtn');
-            var dropdownContent = element.querySelector('.dropdown-content');
-            dropdownBtn.style.color = '';
-            dropdownContent.style.display = 'none';
-        });
-        item.querySelectorAll('div').forEach(child_div => {
-            // Get all div elements
-            const divElements = document.querySelectorAll('.dropdown-content');
-
-            // Iterate over each div element
-            divElements.forEach(div => {
-                if (div.textContent.trim() === child_div.className) {
-                    const element = div.parentNode.parentNode;
-                    console.log(element)
-                    var dropdownBtn = element.querySelector('.dropbtn');
-                    var dropdownContent = element.querySelector('.dropdown-content');
-                    dropdownBtn.style.color = 'red';
-                }
+    document.querySelectorAll('.structure').forEach(item => {
+        item.addEventListener('click', event => {
+            const elements = document.querySelectorAll('.mention_doc_id');
+            elements.forEach(element => {
+                var dropdownBtn = element.querySelector('.dropbtn');
+                var dropdownContent = element.querySelector('.dropdown-content');
+                dropdownBtn.style.color = '';
+                dropdownContent.style.display = 'none';
+            });
+            item.querySelectorAll('div').forEach(child_div => {
+                // Get all div elements
+                const divElements = document.querySelectorAll('.dropdown-content');
+                // Iterate over each div element
+                divElements.forEach(div => {
+                    if (div.textContent.trim() === child_div.className) {
+                        const element = div.parentNode.parentNode;
+                        var dropdownBtn = element.querySelector('.dropbtn');
+                        var dropdownContent = element.querySelector('.dropdown-content');
+                        dropdownBtn.style.color = 'red';
+                    }
+                });
             });
         });
     });
-});
-
 
     function displayResult(result) {
         const content = result.map((list) => {
@@ -64,7 +80,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         resultBox.innerHTML = "<div class='dropdown-content-search'>" + content + "</div>";
     };
 
-    function handleClick(idValue) {
+    function handleClick(idValue, value) {
         if (previousElement) {
             const elements = document.querySelectorAll(`#${previousElement}.mention_doc_id`);
             elements.forEach(element => {
@@ -75,13 +91,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
         }
         const sanitizedId = idValue.replace(/\s/g, '').replace(".", ''); // Remove spaces and "." from the ID
-        console.log(document.querySelectorAll(`#${sanitizedId}`))
         const elements = document.querySelectorAll(`#${sanitizedId}.mention_doc_id`);
         elements.forEach(element => {
             var dropdownBtn = element.querySelector('.dropbtn');
             var dropdownContent = element.querySelector('.dropdown-content');
             dropdownBtn.style.color = 'red';
-            dropdownContent.style.display = 'block';
+            if (value == 2) {
+                dropdownContent.style.display = 'block';
+            }
+
+            // Scroll the current element into view smoothly, aligning it to the center
             element.scrollIntoView({
                 behavior: 'smooth',
                 block: 'center'
@@ -90,10 +109,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
         previousElement = sanitizedId;
     }
 
+    document.addEventListener('click', function(event) {
+    // Selectors to exclude
+    const selectors = [
+        '.structure',
+        '.mention_search_doc_id',
+        '.dropbtn'
+    ];
+
+    // Check if the click occurred on any of the excluded elements
+    const clickedExcludedElement = selectors.some(selector => event.target.closest(selector));
+
+    if (!clickedExcludedElement) {
+        // Handle clicks outside the specified elements
+        const elements = document.querySelectorAll('.mention_doc_id');
+            elements.forEach(element => {
+                var dropdownBtn = element.querySelector('.dropbtn');
+                var dropdownContent = element.querySelector('.dropdown-content');
+                dropdownBtn.style.color = '';
+                dropdownContent.style.display = 'none';
+            })
+    }
+});
+
+
     document.querySelectorAll('.mention_doc_id').forEach(item => {
         item.addEventListener('click', event => {
+            const elements = document.querySelectorAll('.mention_doc_id');
+            elements.forEach(element => {
+                var dropdownBtn = element.querySelector('.dropbtn');
+                var dropdownContent = element.querySelector('.dropdown-content');
+                dropdownBtn.style.color = '';
+                dropdownContent.style.display = 'none';
+            });
             const idValue = item.getAttribute('id');
-            handleClick(idValue);
+            handleClick(idValue, 2);
         });
     });
 });
