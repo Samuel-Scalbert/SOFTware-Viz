08489b5cc9a2e35025e2e1765542fcdf95326c16
Samuel-Scalbert <sam.scalbert@gmail.com>
2024-12-03 11:53:01 +0100

    remove old route `doc/`

--- app/routes/doc_route.py
+++ app/routes/doc_route.py
@@ -2,16 +2,6 @@ from app.app import app, db
 from Utils.doc import doc_info_from_id, doc_software
 from flask import render_template
 
-@app.route('/doc')
-def doc_url():
-    # Assuming you have imported and set up your ArangoDB connection properly
-    all_doc_ids = db.AQLQuery('FOR doc in documents RETURN doc.file_hal_id', rawResults=True)
-    all_urls = []
-    for doc_id in all_doc_ids:
-        url = f'http://127.0.0.1:5000/doc/{doc_id}'
-        all_urls.append(f'<a href="{url}">{doc_id}</a>')
-    return '<br>'.join(all_urls)
-
 @app.route('/doc/<doc_id>')
 def doc_info(doc_id):
     data = doc_info_from_id(doc_id,db)
